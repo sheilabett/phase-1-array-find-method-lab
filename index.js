52ef2f2c5de732adfb89f690e59c18a857248ba6
sheilabett <sheilahbett@gmail.com>
2023-09-28 01:19:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(entry => entry.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+
+  const superBowlRecords = [
+  ];
+
+  console.log(superbowlWin(superBowlRecords));
